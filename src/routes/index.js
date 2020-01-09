054d9d9b1405051bb257dca2270b7351c641857f
Dawoon <rhdrlvnd@ajou.ac.kr>
2020-01-09 12:58:05 +0900

    resolve CORS error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.status(200).json(
     {
       "success" : true
