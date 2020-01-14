f0dba0e212810564cc27adb2c138f2a73e6186a0
YisooKim <gashe7@gmail.com>
2020-01-15 08:44:28 +0900

    p

--- src/routes/search.js
+++ src/routes/search.js
@@ -213,11 +213,11 @@ router.post('/',(req,res)=>{
 			num = listOfKey[0]
 		}
 	}
-	res.redirect('/search/place')
+	res.redirect('/search')
 	
 })
 
-router.get('/place',(req,res)=>{
+router.get('/',(req,res)=>{
 
 	switch(category){
 		case "title":
